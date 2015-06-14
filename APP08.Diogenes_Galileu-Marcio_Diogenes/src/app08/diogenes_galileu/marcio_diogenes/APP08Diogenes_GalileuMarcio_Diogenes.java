76eccc27a92d6b91d80fadbfa9191f4a72a9ac6f
Marushio <marcio.olivercross@gmail.com>
2015-06-14 13:11:08 -0300

    InicioProjeto

---
+++ APP08.Diogenes_Galileu-Marcio_Diogenes/src/app08/diogenes_galileu/marcio_diogenes/APP08Diogenes_GalileuMarcio_Diogenes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app08.diogenes_galileu.marcio_diogenes;
+
+/**
+ *
+ * @author Marcio
+ */
+public class APP08Diogenes_GalileuMarcio_Diogenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
